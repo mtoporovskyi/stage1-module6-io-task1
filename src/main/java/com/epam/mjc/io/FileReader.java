3c8900f9fb02731a293292111053c9c270d3da09
Myroslav Toporovskyi <mtoporovskyy@gmail.com>
2024-01-08 19:45:13 +0200

    stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
+import java.util.HashMap;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String line = null;
+        HashMap<String, String> map = new HashMap<>();
+
+        try (BufferedReader in = new BufferedReader(new java.io.FileReader(file))) {
+            while ((line = in.readLine()) != null) {
+                String[] keyValuePair = line.split(":", 2);
+                if (keyValuePair.length > 1) {
+                    map.put(keyValuePair[0].trim(), keyValuePair[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return this.generateProfile(map);
+    }
+
+    private Profile generateProfile(HashMap<String, String> map) {
+        Profile profile = new Profile();
+        profile.setName(map.get("Name"));
+        profile.setAge(Integer.valueOf(map.get("Age")));
+        profile.setEmail(map.get("Email"));
+        profile.setPhone(Long.valueOf(map.get("Phone")));
+
+        return profile;
     }
 }
